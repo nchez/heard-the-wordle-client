73fbaca884ce021f72e827144226cc10e3f291e8
mvanzo <97002794+mvanzo@users.noreply.github.com>
2022-03-31 19:06:26 -0700

    frontend bugs and dotenv for client id

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,14 +1,15 @@
+import { AiFillLinkedin } from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
+
 export default function Footer() {
     return (
-        <footer className="footer navbar fixed-bottom" style={{ marginTop: '2.5rem' }}>
-
-            <p className='center' style={{ color: 'white' }}>Website Created By:
-                <a style={{ paddingLeft: '10px', 'text-decoration': 'none' }} href="https://www.linkedin.com/in/clair-choo/" target="_blank">Clair Choo</a>,
-                <a style={{ paddingLeft: '10px', 'text-decoration': 'none' }} href="https://www.linkedin.com/in/jon-dmclngn/" target="_blank">Jon Dimaculangan</a>,
-                <a style={{ paddingLeft: '10px', 'text-decoration': 'none' }} href="https://www.linkedin.com/in/nathan-chesmar/" target="_blank">Nate Chesmar</a>,
-                <a style={{ paddingLeft: '10px', 'text-decoration': 'none' }} href="https://www.linkedin.com/in/marty-vanzo/" target="_blank">Marty Vanzo</a>
+        <footer className="footer navbar fixed-bottom pt-3" style={{ marginTop: '2.5rem' }}>
+            <p style={{ color: 'black', fontSize: '14px' }}><span style={{ display:'align-center', color: 'white', fontSize: '15px' }}>Website Created By: </span>
+                Clair Choo <a href="https://www.linkedin.com/in/clair-choo/" target="_blank"><AiFillLinkedin /></a><a href="http://github.com" target="_blank"><AiFillGithub/></a> ||
+                Jon Dimaculangan <a href="https://www.linkedin.com/in/jon-dmclngn/" target="_blank"><AiFillLinkedin /></a><a href="http://github.com" target="_blank"><AiFillGithub/></a> || 
+                Nate Chesmar <a href="https://www.linkedin.com/in/nathan-chesmar/" target="_blank"><AiFillLinkedin /></a><a href="http://github.com" target="_blank"><AiFillGithub/></a> || 
+                Marty Vanzo <a href="https://www.linkedin.com/in/marty-vanzo/" target="_blank"><AiFillLinkedin /></a><a href="http://github.com" target="_blank"><AiFillGithub/></a>
             </p>
-
         </footer>
     )
 }
