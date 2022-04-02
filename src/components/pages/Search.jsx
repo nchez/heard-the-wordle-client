4bf8c077fd185344133c5d9eb0247fac3b198a70
nchez <nathan.chesmar@gmail.com>
2022-04-01 18:07:38 -0700

    fix redirect uri to rankifygame.netlify.app.search

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -4,7 +4,7 @@ import axios from "axios"
 
 export default function Search({ spotifyToken, setSpotifyToken, setDifficulty, difficulty }) {
     // spotify auth details
-    const REDIRECT_URI = "https://famous-gumption-d4f17e.netlify.app/search"
+    const REDIRECT_URI = "https://rankifygame.netlify.app/search"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
 
